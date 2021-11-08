d33bf1a30de5674058abc9bdd73fc6917d8fe0d6
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-08 18:36:39 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/GatewayApplication.java
@@ -0,0 +1,41 @@
+package com.careerdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+@RestController
+public class GatewayApplication {
+
+	private static final String MY_API_KEY = "VD4aSgn9cxh7HPv0NQ0ygecENulZjl182e8mAsNX";
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+ 	@GetMapping("/")
+	public String rootRoute() {
+		return "Welcome Home!";
+	}
+
+	@GetMapping("/apod")
+	public APOD apodInfo(RestTemplate restTemplate) {
+		String URL = "https://api.nasa.gov/planetary/apod?api_key=" + MY_API_KEY;
+
+		APOD apod = restTemplate.getForObject(URL, APOD.class);
+
+		return apod;
+	}
+
+}
