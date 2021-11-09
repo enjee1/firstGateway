0b9b08d5e5a8276d7186c7cc15f20c29045f82d7
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-08 20:29:46 -0500

    Added @JsonInclude to APOD class. Added copyright, hdurl, and date fields to APOD.

--- src/main/java/com/careerdevs/APOD.java
+++ src/main/java/com/careerdevs/APOD.java
@@ -1,10 +1,16 @@
 package com.careerdevs;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class APOD {
 
     private String title;
     private String url;
     private String explanation;
+    private String copyright;
+    private String hdurl;
+    private String date;
 
     public String getTitle() {
         return title;
@@ -28,5 +34,29 @@ public class APOD {
 
     public void setExplanation(String explanation) {
         this.explanation = explanation;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public void setHdurl(String hdurl) {
+        this.hdurl = hdurl;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
